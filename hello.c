571b7d1314e07e62ce54e30124cc6f4539a3b85d
masonhammock <mason.hammock@huskers.unl.edu>
2019-01-17 13:13:15 -0600

    Create hello.c
    Changed Hello Word to Mason Hammock and added a line that states my major.

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Mason Hammock, Makenzy Nagel
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mason Hammock\n");
+  printf("Mechanical Engineering\n");
+
+  return 0;
+}
